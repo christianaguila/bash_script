8910d347a13ddb23b8903df2fbace9a756bc6088
Christian Rhomel Aguila <christianrhomel@gmail.com>
2022-11-13 23:17:07 +0800

    updated

--- eng_econ_calculator.sh
+++ eng_econ_calculator.sh
@@ -61,16 +61,76 @@ ft_amnt_cmpd(){
 	a. Annually
 	b. Semi-Annually
 	c. Quarterly
-	d. Monthly
+	d. Monthly"
 	echo "Enter the letter: "
 	read ans
 
 	case $ans in
-	a) ft_amnt=$(awk "BEGIN {}")
+		a) m=1;;
+		b) m=2;;
+		c) m=4;;
+		d) m=12;;
+	esac
 
-        ft_amnt=$(awk "BEGIN {print $p(1+($i/100)*$n)}")
+        ft_amnt=$(awk "BEGIN {print $p(1+($i/m*100))**($n*$m)}")
         echo "Future Amount: $ft_amnt"
 }
+
+
+pr_amnt_cmpd(){
+        echo "How much is the future amount?"
+        read p
+        echo "What is the interest rate(%)?"
+        read i
+        echo "How many years?"
+        read n
+        echo "What is the method of compounding?
+        a. Annually
+        b. Semi-Annually
+        c. Quarterly
+        d. Monthly"
+        echo "Enter the letter: "
+        read ans
+
+        case $ans in
+                a) m=1;;
+                b) m=2;;
+                c) m=4;;
+                d) m=12;;
+        esac
+
+        pr_amnt=$(awk "BEGIN {print $f(1+($i/m*100))**(-$n*$m)}")
+        echo "Principal Amount: $pr_amnt"
+}
+
+
+int_rt_cmpd(){
+        echo "How much is the future amount?"
+        read f
+        echo "What is the principal amount"
+        read p
+        echo "How many years?"
+        read n
+        echo "What is the method of compounding?
+        a. Annually
+        b. Semi-Annually
+        c. Quarterly
+        d. Monthly"
+        echo "Enter the letter: "
+        read ans
+
+        case $ans in
+                a) m=1;;
+                b) m=2;;
+                c) m=4;;
+                d) m=12;;
+        esac
+
+        int_rt=$(awk "BEGIN {print ($m(($f/$p)**(1/$n*$m)-$m))*100 %}")
+        echo "Interest Rate: $int_rt"
+}
+
+
 
 
 echo "What are you trying to solve?
@@ -102,6 +162,9 @@ else
 	read ch
 
 	case $ch in
-		a) 
+		a) ft_amnt_cmpd;;
+		b) pr_amnt_cmpd;;
+		c) int_rt_cmpd;;
+	esac
 fi
 
